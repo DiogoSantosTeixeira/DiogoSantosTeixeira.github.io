4789367e60ca3ebb5f15be6ccf0b3d5b07f71fac
DiogoSantosTeixeira <153910769+DiogoSantosTeixeira@users.noreply.github.com>
2024-03-12 12:46:35 +0000

    added theme toggle

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,24 @@
-const observer = new IntersectionObserver((entries) => {
-    entries.forEach((entry) => {
-        if (entry.isIntersecting) {
-            entry.target.classList.add("show");
-        }
-    });
+const observer = new IntersectionObserver(entries => {
+	entries.forEach(entry => {
+		if (entry.isIntersecting) {
+			entry.target.classList.add('show');
+		}
+	});
+});
+const hiddenEl = document.querySelectorAll('.hidden');
+
+const toggleButton = document.querySelector('.navbar__toggle');
+const theme = localStorage.getItem('theme');
+
+theme && document.body.classList.add(theme);
+
+toggleButton.addEventListener('click', () => {
+	document.body.classList.toggle('dark-mode');
+	if (document.body.classList.contains('dark-mode')) {
+		localStorage.setItem('theme', 'dark-mode');
+	} else {
+		localStorage.removeItem('theme');
+	}
 });
 
-const hiddenEl = document.querySelectorAll(".hidden");
-
-
-
-hiddenEl.forEach((el) => observer.observe(el));
+hiddenEl.forEach(el => observer.observe(el));
