2bfbbdb79610c077cce762d0249fd85adb4b75fd
DiogoSantosTeixeira <diogo.sleepy@gmail.com>
2024-03-13 16:51:05 +0000

    worked on reviews layout for mobile, added arrows for navigation

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,13 +13,29 @@ const theme = localStorage.getItem('theme');
 const menuButton = document.querySelector('.navbar__hamburguer');
 const menuEl = document.querySelector('.navbar__menu');
 
+const prevButton = document.querySelector('.prev_button');
+const nextButton = document.querySelector('.next_button');
+const card = document.querySelector('.reviews__card');
+const cardsContainer = document.querySelector('.reviews__cards');
+
+nextButton.addEventListener('click', () => {
+	const cardWidth = card.clientWidth + 4;
+	cardsContainer.scrollLeft += cardWidth;
+});
+
+prevButton.addEventListener('click', () => {
+	const cardWidth = card.clientWidth + 4;
+	cardsContainer.scrollLeft -= cardWidth;
+	console.log(cardWidth);
+});
+
 menuButton.addEventListener('click', () => {
 	if (menuEl.classList.contains('active')) {
 		menuEl.classList.remove('active');
 	} else {
 		menuEl.classList.add('active');
 	}
-})
+});
 
 theme && document.body.classList.add(theme);
 
